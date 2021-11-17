9645c45e26ce0f132f13419630cb7292aeb15a53
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-11-17 15:12:59 +0530

    added rest client

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.get('/', (req, res) => {
-    res.send('First Request');
-})
+app.get("/", (req, res) => {
+  res.send("First Request");
+});
 
-app.get('/users', (req, res)=>{
-    res.send("Here are users!")
-})
+app.get("/users", (req, res) => {
+  let users = ["venky", "tharun", "pranav", "pushpa"];
+  res.send({ users: users });
+});
 
-app.listen(3000, ()=>{
-    console.log("Listening to Port 3000");
+app.listen(3000, () => {
+  console.log("Listening to Port 3000");
 });
