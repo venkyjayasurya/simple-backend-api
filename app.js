0087c0f31a8473197d2920c4aaab6c77e000bfde
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-11-17 15:08:35 +0530

    users endpoint

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ app.get('/', (req, res) => {
     res.send('First Request');
 })
 
+app.get('/users', (req, res)=>{
+    res.send("Here are users!")
+})
+
 app.listen(3000, ()=>{
     console.log("Listening to Port 3000");
 });
