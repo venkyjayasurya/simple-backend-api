5a53b41971967acf3d418be4cd7e9cb8da53fc1e
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-11-17 15:22:42 +0530

    added create user endpoint

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+
+// const customMiddleware = (req, res, next) => {
+//     console.log("Welcome to middleware")
+//     next();
+// }
+// app.use(customMiddleware);
+
 app.get("/", (req, res) => {
   res.send("First Request");
 });
@@ -8,6 +15,10 @@ app.get("/users", (req, res) => {
   let users = ["venky", "tharun", "pranav", "pushpa"];
   res.send({ users: users });
 });
+
+app.post('/createUser', (req, res)=>{
+    console.log(req);
+})
 
 app.listen(3000, () => {
   console.log("Listening to Port 3000");
